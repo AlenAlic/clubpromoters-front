c29e3e0cc35e46989fb2992e6903b941cb8d7459
Alen Alic <aalic89@gmail.com>
2020-06-09 17:30:07 +0200

    add app-service-worker to catch prompt

---
+++ src/components/app/AppServiceWorker.vue
@@ -0,0 +1,37 @@
+<script>
+/* eslint-disable no-console */
+export default {
+  created() {
+    let installPrompt;
+
+    window.addEventListener("beforeinstallprompt", e => {
+      e.preventDefault();
+      installPrompt = e;
+      // setTimeout(() => {
+      //   this.sheet = true;
+      // }, 1000);
+    });
+
+    this.installer = () => {
+      installPrompt.prompt();
+      installPrompt.userChoice.then(result => {
+        if (result.outcome === "accepted") {
+          console.log("Install accepted!");
+        } else {
+          console.log("Install denied!");
+        }
+      });
+    };
+  },
+  methods: {
+    installSW() {
+      console.log("Install accepted!");
+      this.sheet = false;
+    },
+    cancelSW() {
+      console.log("Install cancelled!");
+      this.sheet = false;
+    }
+  }
+};
+</script>
