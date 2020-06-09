ffc0fd767effd7c1a61e2c140e32715742df493c
Alen Alic <aalic89@gmail.com>
2020-06-09 21:29:21 +0200

    add download codes in excel

---
+++ src/components/organizer/codes/PrintCodesCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-card>
+    <v-card-title>{{ $t("organizer.download_codes.title") }}</v-card-title>
+    <v-card-text>{{ $t("organizer.download_codes.text") }}</v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary" text @click="dialog = true">
+        {{ $t("organizer.download_codes.select") }}
+      </v-btn>
+    </v-card-actions>
+    <modal v-if="dialog" :show="dialog">
+      <v-form @submit.prevent="printCodes">
+        <v-card>
+          <v-card-title>
+            <span>{{ $t("organizer.download_codes.title") }}</span>
+            <v-spacer />
+            <v-btn icon class="float-right" @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-subtitle>{{ $t("organizer.download_codes.modal.subtitle") }}</v-card-subtitle>
+          <v-card-text v-if="!loading">
+            <v-checkbox
+              v-for="code in this.$store.state.codes.activeCodes"
+              :key="code.id"
+              v-model="codes"
+              :label="`${code.code}${code.promoter ? ' - ' : ''}${code.promoter ? code.promoter.name : ''}`"
+              :value="code.id"
+              dense
+              hide-details
+              multiple
+            />
+          </v-card-text>
+          <v-card-text v-else>
+            {{ $t("organizer.download_codes.modal.loading") }}
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn :loading="loading" color="primary" text type="submit">
+              {{ $t("organizer.download_codes.modal.submit") }}
+            </v-btn>
+            <v-btn text @click="dialog = false">
+              {{ $t("general.cancel") }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </modal>
+  </v-card>
+</template>
+
+<script>
+import Modal from "@/components/modal/Modal";
+export default {
+  components: { Modal },
+  data: function() {
+    return {
+      loading: false,
+      dialog: false,
+      codes: []
+    };
+  },
+  methods: {
+    printCodes() {
+      this.loading = true;
+      this.axios
+        .post(
+          "organizer/codes/excel",
+          {
+            codes: this.codes
+          },
+          {
+            responseType: "blob"
+          }
+        )
+        .then(response => {
+          let fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          let fileLink = document.createElement("a");
+
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", `codes_${this.$util.now.toFormat("dd-LL-yyyy_HHmmss")}.xlsx`);
+          document.body.appendChild(fileLink);
+
+          fileLink.click();
+          this.dialog = false;
+          this.codes = [];
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    }
+  }
+};
+</script>
--- src/pages/organizer/Codes.vue
+++ src/pages/organizer/Codes.vue
@@ -2,7 +2,8 @@
   <div>
     <v-row>
       <v-col cols="12" lg="3">
-        <create-new-codes-card />
+        <create-new-codes-card class="mb-5" />
+        <print-codes-card />
       </v-col>
       <v-col cols="12" lg="5">
         <view-active-codes-card />
@@ -17,7 +18,8 @@
 import CreateNewCodesCard from "@/components/organizer/codes/CreateNewCodesCard";
 import ViewActiveCodesCard from "@/components/organizer/codes/ViewActiveCodesCard";
 import ViewInactiveCodesCard from "@/components/organizer/codes/ViewInactiveCodesCard";
+import PrintCodesCard from "@/components/organizer/codes/PrintCodesCard";
 export default {
-  components: { ViewInactiveCodesCard, ViewActiveCodesCard, CreateNewCodesCard }
+  components: { PrintCodesCard, ViewInactiveCodesCard, ViewActiveCodesCard, CreateNewCodesCard }
 };
 </script>
