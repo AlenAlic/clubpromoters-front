8b5bb2c6f55348f1542c7968469ac4cedcee0970
Alen Alic <aalic89@gmail.com>
2020-07-15 12:46:30 +0200

    show no code card when promoters has no code

--- src/pages/promoter/Code.vue
+++ src/pages/promoter/Code.vue
@@ -6,7 +6,11 @@
           <v-img :src="`data:image/png;base64,${$store.state.code.code.qr_code}`" alt="QR code"></v-img>
           <div class="text-center pb-5 display-1">{{ $store.state.code.code.code }}</div>
         </div>
-        <v-skeleton-loader v-else class="mx-auto" max-width="400" type="card"></v-skeleton-loader>
+        <v-skeleton-loader v-else-if="$store.state.code.loading" class="mx-auto" max-width="400" type="card" />
+        <template v-else>
+          <v-card-title>{{ $t("promoter.no_code.title") }}</v-card-title>
+          <v-card-text>{{ $t("promoter.no_code.text") }}</v-card-text>
+        </template>
       </v-card>
     </v-col>
   </v-row>
