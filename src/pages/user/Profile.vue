c29588f78a98f8b53b99e74c121fb8402ea87517
Alen Alic <aalic89@gmail.com>
2020-07-21 14:42:36 +0200

    hide profile items for organizer account

--- src/pages/user/Profile.vue
+++ src/pages/user/Profile.vue
@@ -9,43 +9,45 @@
           class="mb-4"
         ></v-boilerplate>
       </v-col>
-      <v-col col="12" md="6" lg="4" xl="3">
-        <address-card v-if="$store.state.auth.profile" />
-        <v-boilerplate
-          v-else-if="$store.state.auth.loadingProfile"
-          :skeletons="[
-            'card-heading',
-            'list-item-two-line',
-            'list-item-two-line',
-            'list-item-two-line',
-            'list-item-two-line',
-            'list-item-two-line',
-            'actions'
-          ]"
-        ></v-boilerplate>
-      </v-col>
-      <v-col col="12" md="6" lg="4" xl="3">
-        <invoice-card v-if="$store.state.auth.profile" />
-        <v-boilerplate
-          v-else-if="$store.state.auth.loadingProfile"
-          :skeletons="[
-            'card-heading',
-            'list-item-two-line',
-            'list-item-two-line',
-            'list-item-two-line',
-            'list-item-two-line',
-            'actions'
-          ]"
-        ></v-boilerplate>
-      </v-col>
-      <v-col col="12" md="6" lg="4" xl="3">
-        <invoice-language-card v-if="$store.state.auth.profile" class="mb-4" />
-        <v-boilerplate
-          v-else-if="$store.state.auth.loadingProfile"
-          :skeletons="['card-heading', 'list-item-two-line', 'actions']"
-          class="mb-4"
-        ></v-boilerplate>
-      </v-col>
+      <template v-if="showInvoiceItems">
+        <v-col col="12" md="6" lg="4" xl="3">
+          <address-card v-if="$store.state.auth.profile" />
+          <v-boilerplate
+            v-else-if="$store.state.auth.loadingProfile"
+            :skeletons="[
+              'card-heading',
+              'list-item-two-line',
+              'list-item-two-line',
+              'list-item-two-line',
+              'list-item-two-line',
+              'list-item-two-line',
+              'actions'
+            ]"
+          ></v-boilerplate>
+        </v-col>
+        <v-col col="12" md="6" lg="4" xl="3">
+          <invoice-card v-if="$store.state.auth.profile" />
+          <v-boilerplate
+            v-else-if="$store.state.auth.loadingProfile"
+            :skeletons="[
+              'card-heading',
+              'list-item-two-line',
+              'list-item-two-line',
+              'list-item-two-line',
+              'list-item-two-line',
+              'actions'
+            ]"
+          ></v-boilerplate>
+        </v-col>
+        <v-col col="12" md="6" lg="4" xl="3">
+          <invoice-language-card v-if="$store.state.auth.profile" class="mb-4" />
+          <v-boilerplate
+            v-else-if="$store.state.auth.loadingProfile"
+            :skeletons="['card-heading', 'list-item-two-line', 'actions']"
+            class="mb-4"
+          ></v-boilerplate>
+        </v-col>
+      </template>
     </v-row>
   </v-container>
 </template>
@@ -55,6 +57,7 @@ import ProfileCard from "@/components/user/ProfileCard";
 import AddressCard from "@/components/user/AddressCard";
 import InvoiceLanguageCard from "@/components/user/InvoiceLanguageCard";
 import InvoiceCard from "@/components/user/InvoiceCard";
+import { CLUB_OWNER, PROMOTER } from "@/constants";
 export default {
   components: {
     InvoiceCard,
@@ -62,6 +65,11 @@ export default {
     AddressCard,
     VBoilerplate,
     ProfileCard
+  },
+  computed: {
+    showInvoiceItems() {
+      return this.$store.getters.access === CLUB_OWNER || this.$store.getters.access === PROMOTER;
+    }
   }
 };
 </script>
