7007114dff9f55b0a2a458b20f6717c98990daf1
Alen Alic <aalic89@gmail.com>
2020-07-31 20:41:06 +0200

    add dutch language

--- src/languages/nl/index.js
+++ src/languages/nl/index.js
@@ -1,8 +1,44 @@
 // Please add the imports in alphabetical order for ease of finding when things become large.
+import admin from "./admin";
+import auth from "./auth";
+import club_owner from "./club_owner";
+import errors from "./errors";
+import form from "./form";
+import general from "./general";
+import hostess from "./hostess";
+import invoices from "./invoices";
 import locale from "./locale";
+import menu from "./menu";
+import networkErrors from "./network-errors";
+import organizer from "./organizer";
+import page_not_found from "./page_not_found";
+import party_card from "./party_card";
+import password_requirements from "./password_requirements";
+import promoter from "./promoter";
+import purchase from "./purchase";
+import user from "./user";
+import vuetify from "./vuetify";
 
 const nl = {
-  ...locale
+  ...admin,
+  ...auth,
+  ...club_owner,
+  ...errors,
+  ...form,
+  ...general,
+  ...hostess,
+  ...invoices,
+  ...locale,
+  ...menu,
+  ...networkErrors,
+  ...organizer,
+  ...page_not_found,
+  ...party_card,
+  ...password_requirements,
+  ...promoter,
+  ...purchase,
+  ...user,
+  ...vuetify
 };
 
 export default nl;
